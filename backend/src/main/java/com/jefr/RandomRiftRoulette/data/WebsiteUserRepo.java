5881d38f91401f8b2283ea9e63aac329b4a0611b
JefrDev <94841564+JefrDev@users.noreply.github.com>
2025-01-25 14:29:46 +0100

    gitignore application.properties

--- backend/src/main/java/com/jefr/RandomRiftRoulette/data/WebsiteUserRepo.java
+++ backend/src/main/java/com/jefr/RandomRiftRoulette/data/WebsiteUserRepo.java
@@ -4,6 +4,6 @@ import com.jefr.RandomRiftRoulette.domain.WebsiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+    @Repository
     public interface WebsiteUserRepo extends JpaRepository<WebsiteUser, String> {
 }
